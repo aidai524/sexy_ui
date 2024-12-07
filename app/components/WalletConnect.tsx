433116d8f1c4f1d27da1bbff78d9588f1c7eae13
jimmygu <jimmyguu2024@gmail.com>
2024-12-07 17:26:14 +0800

    test: wallet styles

--- app/components/WalletConnect.tsx
+++ app/components/WalletConnect.tsx
@@ -1,47 +1,13 @@
 'use client'
 
 import React, { useMemo } from 'react';
-import { createAppKit, useAppKitProvider, useWalletInfo } from '@reown/appkit/react'
-import { SolanaAdapter, useAppKitConnection } from '@reown/appkit-adapter-solana/react'
-import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks'
-import { PhantomWalletAdapter, SolflareWalletAdapter,  } from '@solana/wallet-adapter-wallets'
+import { PhantomWalletAdapter, SolflareWalletAdapter, WalletConnectWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
-
-// const solanaWeb3JsAdapter = new SolanaAdapter({
-//   // @ts-ignore
-//   wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
-// })
-
-
-// const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID || '0b2f56ac0fc402ed0e62c10d7e75f555'
-// const metadata = {
-//   name: 'Dating App',
-//   description: 'A Solana-based dating app',
-//   url: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
-//   icons: ['https://picsum.photos/200/200']
-// }
-
-// const apKit = createAppKit({
-//   adapters: [solanaWeb3JsAdapter],
-//   networks: [solanaDevnet],
-//   metadata: metadata,
-//   projectId,
-//   features: {
-//     analytics: true
-//   }
-// });
-
-// apKit.subscribeEvents((e) => {
-//   console.log('>>>>> subscribeEvents', e.data);
-//   if (e.data.event === "CONNECT_SUCCESS" && e.data.properties.name === 'Phantom' && /Mobi|Android/i.test(window?.navigator.userAgent)) {
-//     window.location.href = `https://phantom.app/ul/v1/connect?redirect_link=${window.location.href}`;
-//   }
-// })
 
 export default function WalletConnect({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Mainnet;
--- app/components/connectButton/ConnectButton.tsx
+++ app/components/connectButton/ConnectButton.tsx
@@ -1,70 +1,26 @@
 'use client'
-import React, { useState } from 'react'
-import { Button } from 'antd-mobile'
-import { Provider, SolanaAdapter, useAppKitConnection } from '@reown/appkit-adapter-solana/react'
-import { useAppKit, useAppKitProvider, useAppKitState, useAppKitAccount, useDisconnect, UseAppKitNetworkReturn } from '@reown/appkit/react'
-import {
-  Keypair,
-  Connection,
-  PublicKey,
-  LAMPORTS_PER_SOL,
-  SystemProgram,
-  TransactionInstruction,
-  Transaction,
-  sendAndConfirmTransaction,
-} from '@solana/web3.js';
+
+import React from 'react'
 import styles from './connectButton.module.css'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { WalletModalButton } from '@solana/wallet-adapter-react-ui';
 import { useWallet } from '@solana/wallet-adapter-react';
 
 export default function ConnectButton() {
-  // const appKit = useAppKit()
-  // const { walletProvider } = useAppKitProvider<Provider>('solana')
-  // const account = useAppKitAccount()
-  // const { disconnect, } = useDisconnect()
-  // const { connection } = useAppKitConnection()
   const { connected, publicKey, disconnect } = useWallet();
-
-  // console.log('connection:', connection)
-
-  // const handleConnect = async () => {
-  //   try {
-  //     // await appKit.open()
-  //   } catch (error) {
-  //     console.error('Wallet connection error:', error)
-  //   } finally {
-  //   }
-  // }
-  //
-  // const handleDisconnect = async () => {
-  //   // disconnect()
-  //   // await appKit.open()
-  // }
-
-  // console.log('account:', account)
-
-  // if (account && account.address) {
-  //   return <div onClick={handleDisconnect}>
-  //     <img src="/img/home/solana.png" className={styles.loginBtn} />
-  //   </div>
-  // }
 
   return (
     <div>
       {
         connected ? (
           <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
-            {/*<div className="">
-              {publicKey?.toString?.()?.slice(0, 4) + '...' + publicKey?.toString?.()?.slice(39)}
-            </div>*/}
             <div className={styles.button} onClick={disconnect}>
               Disconnect
             </div>
           </div>
         ) : (
-          <WalletMultiButton style={{ background: '#FFFFFF1F', borderRadius: 24 }}>
+          <WalletModalButton style={{ background: '#FFFFFF1F', borderRadius: 24 }}>
             Connect
-          </WalletMultiButton>
+          </WalletModalButton>
         )
       }
     </div>
