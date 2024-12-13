68455dcba989463c99bff996da4dbc5c941c8759
Amy0104 <amywang0104@163.com>
2024-12-13 15:29:56 +0800

    add token info.

--- app/sections/home/laptop/main/token/index.tsx
+++ app/sections/home/laptop/main/token/index.tsx
@@ -2,11 +2,13 @@ import ActionsBar from "../actions-bar/prelaunch";
 import styles from "./index.module.css";
 import Header from "./header";
 import TokenCard from "../token-card";
+import InfoPart from "@/app/sections/detail/components/info/infoPart";
+import CommentComp from "@/app/components/comment";
 import useData from "../../../hooks/use-data";
 
 export default function Token() {
-  const { infoData2, onLike, onHate, getnext } = useData('preLaunch');
-  console.log(infoData2);
+  const { infoData2, onLike, onHate, getnext } = useData("preLaunch");
+
   const like = () => {
     setTimeout(() => {
       getnext();
@@ -27,6 +29,19 @@ export default function Token() {
         <Header tokenInfo={infoData2} />
         <div className={styles.Content}>
           <TokenCard token={infoData2} />
+          {infoData2 && (
+            <div className={styles.PanelWrapper}>
+              <InfoPart
+                showLikes={false}
+                data={infoData2}
+                showThumbnailHead={false}
+                showTop={false}
+                theme="light"
+                sepSize={2}
+              />
+              <CommentComp id={infoData2.id} theme="light" />
+            </div>
+          )}
         </div>
       </div>
       <ActionsBar tokenInfo={infoData2} onLike={like} onHate={hate} />
