bceb3c8f11faa5eedad51a8a3a23e9ea70e514b9
emily <emily@ref.finance>
2024-12-20 16:18:48 +0800

    add to home screen.

--- app/index.d.ts
+++ app/index.d.ts
@@ -5,4 +5,6 @@ interface Window {
       auth: any;
     };
   };
+  AddToHomeScreenInstance: any;
+  AddToHomeScreen: any;
 }
--- next.config.js
+++ next.config.js
@@ -1,71 +1,69 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['antd-mobile'],
-  output: 'standalone',
-  
+  reactStrictMode: false,
+  transpilePackages: ["antd-mobile"],
+  output: "standalone",
 
   compress: true,
   rewrites: async () => [
     {
-      source: '/api/v1/:path*',
-      destination: 'https://api.dumpdump.fun/api/v1/:path*'
-    },
+      source: "/api/v1/:path*",
+      destination: "https://api.dumpdump.fun/api/v1/:path*"
+    }
   ],
 
   webpack: (config, { dev, isServer }) => {
-
     if (!dev && !isServer) {
       config.optimization = {
         ...config.optimization,
         minimize: true,
         splitChunks: {
-          chunks: 'all',
+          chunks: "all",
           minSize: 20000,
-          maxSize: 24000000, 
+          maxSize: 24000000,
           cacheGroups: {
             default: false,
             vendors: false,
 
             framework: {
-              chunks: 'all',
-              name: 'framework',
+              chunks: "all",
+              name: "framework",
               test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
               priority: 40,
-              enforce: true,
+              enforce: true
             },
 
             antd: {
-              chunks: 'all',
-              name: 'antd',
+              chunks: "all",
+              name: "antd",
               test: /[\\/]node_modules[\\/](@ant-design|antd|antd-mobile)[\\/]/,
               priority: 30,
-              enforce: true,
+              enforce: true
             },
 
             lib: {
               test: /[\\/]node_modules[\\/]/,
-              name: 'lib',
-              priority: 20,
-            },
+              name: "lib",
+              priority: 20
+            }
           }
         }
-      }
+      };
     }
-    return config
+    return config;
   },
   images: {
-    domains: ['picsum.photos'], 
+    domains: ["picsum.photos"],
     minimumCacheTTL: 60,
-    formats: ['image/webp'],
+    formats: ["image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
-    ],
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**"
+      }
+    ]
   }
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
