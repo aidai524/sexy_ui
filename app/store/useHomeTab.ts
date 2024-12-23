51f03927d03658ad13f8bc7665b70d952e3219ca
ben <benjm240@gmail.com>
2024-12-23 09:46:55 +0800

    add homeindex strore

---
+++ app/store/useHomeTab.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+export const useHomeTab = create(
+    persist(
+        (set, get: any) => ({
+            homeTabIndex: 0,
+            set: (params: any) => set(() => ({ ...params })),
+        }),
+        {
+            name: '_home_tab',
+            version: 0.1,
+            storage: createJSONStorage(() => localStorage)
+        }
+    )
+);
