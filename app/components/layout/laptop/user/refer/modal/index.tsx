20f33f86fc18b31676bfbe7986ca1d6a85da2f33
jimmygu <jimmyguu2024@gmail.com>
2024-12-26 15:59:55 +0800

    feat: finished refer page on mobile device

--- app/components/layout/laptop/user/refer/modal/index.tsx
+++ app/components/layout/laptop/user/refer/modal/index.tsx
@@ -3,15 +3,48 @@ import { useReferStore } from '@/app/store/useRefer';
 import Modal from '@/app/components/modal';
 import { fail, success } from '@/app/utils/toast';
 import { useAccount } from '@/app/hooks/useAccount';
+import { Popup } from 'antd-mobile';
 
 const ReferModal = (props: any) => {
-  const { userInfo } = props;
+  const { isMobile } = props;
   const store = useReferStore();
-  const { address } = useAccount();
 
   const handleClose = () => {
     store.setVisible(false);
   };
+
+  return isMobile ? (
+    <Popup
+      visible={store.visible}
+      onMaskClick={handleClose}
+      onClose={handleClose}
+      bodyStyle={{
+        borderTopLeftRadius: "8px",
+        borderTopRightRadius: "8px",
+      }}
+    >
+      <ReferModalContent {...props} />
+    </Popup>
+    ) : (
+    <Modal
+      open={store.visible}
+      onClose={handleClose}
+      style={{
+      }}
+      mainStyle={{
+        border: 0,
+      }}
+    >
+      <ReferModalContent {...props} />
+    </Modal>
+  );
+};
+
+export default ReferModal;
+
+const ReferModalContent = (props: any) => {
+  const { userInfo, isMobile } = props;
+  const { address } = useAccount();
 
   const handleCopy = () => {
     const shareLink = new URL(window?.location?.origin);
@@ -26,97 +59,85 @@ const ReferModal = (props: any) => {
   };
 
   return (
-    <Modal
-      open={store.visible}
-      onClose={handleClose}
-      style={{
-      }}
-      mainStyle={{
-        border: 0,
-      }}
-    >
-      <div className={styles.Container}>
-        <div className={styles.Title}>
-          Referral Earning
-        </div>
-        <div className={styles.Content}>
-          <div className={styles.Earned}>
-            <div className={styles.EarnedTitle}>
-              <div className={styles.EarnedTitleText}>
-                EARNED
-              </div>
-              <div className={styles.EarnedTitleValue}>
-                {userInfo?.referralFee || 0} SOL
-              </div>
+    <div className={isMobile ? styles.ContainerMobile : styles.Container}>
+      <div className={styles.Title}>
+        Referral Earning
+      </div>
+      <div className={isMobile ? styles.ContentMobile : styles.Content}>
+        <div className={isMobile ? styles.EarnedMobile : styles.Earned}>
+          <div className={styles.EarnedTitle}>
+            <div className={styles.EarnedTitleText}>
+              EARNED
             </div>
-            <div className={styles.Progress}>
-              <div className={styles.ProgressValue} style={{ width: '25%' }} />
-              <div className={styles.NodeList}>
-                <div className={styles.NodeActive}>
-                  <div className={styles.NodeLabel}>
-                    Vol.50k
-                  </div>
-                  <div className={styles.NodeReward}>
-                    <div className={styles.NodeRewardLabel}>
-                      Earn Rewards
-                    </div>
-                    <div className={styles.NodeRewardContent}>
-                      <div className={styles.NodeRewardValue}>
-                        0.4 SOL
-                      </div>
-                      <div className={styles.NodeRewardUnit}>
-                        /month
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.Node}>
+            <div className={styles.EarnedTitleValue}>
+              {userInfo?.referralFee || 0} SOL
+            </div>
+          </div>
+          <div className={styles.Progress}>
+            <div className={styles.ProgressValue} style={{ width: '25%' }} />
+            <div className={styles.NodeList}>
+              <div className={styles.NodeActive}>
                 <div className={styles.NodeLabel}>
-                    Vol.250k
-                  </div>
+                  Vol.50k
                 </div>
-                <div className={styles.Node}>
-                  <div className={styles.NodeLabel}>
-                    Vol.500k
+                <div className={styles.NodeReward}>
+                  <div className={styles.NodeRewardLabel}>
+                    Earn Rewards
                   </div>
-                  <div className={styles.NodeReward}>
-                    <div className={styles.NodeRewardLabel}>
-                      Earn Rewards
+                  <div className={styles.NodeRewardContent}>
+                    <div className={styles.NodeRewardValue}>
+                      0.4 SOL
                     </div>
-                    <div className={styles.NodeRewardContent}>
-                      <div className={styles.NodeRewardValue}>
-                        0.8 SOL
-                      </div>
-                      <div className={styles.NodeRewardUnit}>
-                        /month
-                      </div>
+                    <div className={styles.NodeRewardUnit}>
+                      /month
                     </div>
                   </div>
                 </div>
-                <div className={styles.Node}>
-                  <div className={styles.NodeLabel}>
-                    Vol.1000k
+              </div>
+              <div className={styles.Node}>
+                <div className={styles.NodeLabel}>
+                  Vol.250k
+                </div>
+              </div>
+              <div className={styles.Node}>
+                <div className={styles.NodeLabel}>
+                  Vol.500k
+                </div>
+                <div className={styles.NodeReward}>
+                  <div className={styles.NodeRewardLabel}>
+                    Earn Rewards
                   </div>
+                  <div className={styles.NodeRewardContent}>
+                    <div className={styles.NodeRewardValue}>
+                      0.8 SOL
+                    </div>
+                    <div className={styles.NodeRewardUnit}>
+                      /month
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.Node}>
+                <div className={styles.NodeLabel}>
+                  Vol.1000k
                 </div>
               </div>
             </div>
           </div>
-          <div className={styles.Invite}>
-            <div className={styles.InviteText}>
-              Invite Friends and Earn Commissions Get up <br /> to <strong className={styles.InviteTextPrimary}>$5000</strong> rebates
-            </div>
-            <button
-              type="button"
-              className={styles.InviteBtn}
-              onClick={handleCopy}
-            >
-              Invite now
-            </button>
+        </div>
+        <div className={styles.Invite}>
+          <div className={styles.InviteText}>
+            Invite Friends and Earn Commissions Get up {!isMobile && <br />} to <strong className={styles.InviteTextPrimary}>$5000</strong> rebates
           </div>
+          <button
+            type="button"
+            className={styles.InviteBtn}
+            onClick={handleCopy}
+          >
+            Invite now
+          </button>
         </div>
       </div>
-    </Modal>
+    </div>
   );
 };
-
-export default ReferModal;
