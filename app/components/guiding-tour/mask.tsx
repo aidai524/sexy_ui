0e443c93bc57490725aa4c595ec72be92bbe1196
emily <emily@ref.finance>
2024-12-26 18:21:14 +0800

    add guid.

--- app/components/guiding-tour/mask.tsx
+++ app/components/guiding-tour/mask.tsx
@@ -1,4 +1,5 @@
 import React, { CSSProperties, useEffect, useMemo, useState } from "react";
+import { useUserAgent } from "@/app/context/user-agent";
 import { motion } from "framer-motion";
 import {
   MaskPlacement,
@@ -6,12 +7,13 @@ import {
   getMaskBoundRect
 } from "./get-style-rect";
 import { useScrollIntoView } from "@/app/hooks/use-scroll-into-view";
+
 import styles from "./index.module.css";
 
 interface MaskProps {
   element: HTMLElement;
   container?: HTMLElement;
-  renderMaskContent?: (wrapper: React.ReactNode) => React.ReactNode;
+  renderMaskContent?: () => React.ReactNode;
   onAnimationStart?: () => void;
   onAnimationEnd?: () => void;
   placement?: MaskPlacement;
@@ -34,6 +36,7 @@ export const Mask: React.FC<MaskProps> = (props) => {
     reset = false,
     type
   } = props;
+  const { isMobile } = useUserAgent();
 
   const [style, setStyle] = useState<CSSProperties>({});
 
@@ -99,18 +102,23 @@ export const Mask: React.FC<MaskProps> = (props) => {
     };
   }, [element, container, reset, scrollElementIntoView]);
 
-  const getContent = () => {
-    if (!renderMaskContent) {
-      return null;
-    }
-    return renderMaskContent(<div className="w-full h-full relative" />);
-  };
-
   const { top, left, elementWidth, elementHeight } = style as any;
 
   const elementRect = useMemo(() => element.getClientRects()?.[0], [element]);
 
   const triStyle = useMemo(() => {
+    if (isMobile) {
+      switch (placement) {
+        case MaskPlacement.Top:
+          return {
+            bottom: -9,
+            left:
+              elementRect.left -
+              elementWidth / 2 -
+              (contentWidth - elementRect.left < 10 ? 14 : 4)
+          };
+      }
+    }
     if (placement === 0) return { bottom: -9, right: 76 };
     if (placement === 1) return { bottom: -9, right: contentWidth / 2 };
     if (placement === 6)
@@ -125,14 +133,37 @@ export const Mask: React.FC<MaskProps> = (props) => {
         left: contentWidth / 2,
         transform: "rotate(180deg)"
       };
-    if ([8, 9, 10, 11].includes(placement))
-      return {
-        left: -26,
-        top: 20,
-        transform: "rotate(-90deg)"
-      };
     return {};
-  }, [placement]);
+  }, [placement, isMobile, contentWidth, elementWidth, elementRect]);
+
+  const [iconStyle, innerIconStyle] = useMemo(() => {
+    if (element?.id === "guid-home-create-mobile") {
+      return [
+        {
+          width: 80,
+          height: 80,
+          left: elementRect.left - 30,
+          top: elementRect.top - 40
+        },
+        {
+          width: 68,
+          height: 68
+        }
+      ];
+    }
+    return [
+      {
+        width: elementWidth + 26,
+        height: elementWidth + 26,
+        left: elementRect.left - elementWidth / 2,
+        top: elementRect.top - elementWidth / 2
+      },
+      {
+        width: elementWidth + 14,
+        height: elementWidth + 14
+      }
+    ];
+  }, [elementRect, elementWidth]);
 
   return (
     <div className={styles.Mask}>
@@ -156,23 +187,30 @@ export const Mask: React.FC<MaskProps> = (props) => {
           />
         </div>
       ) : (
-        <div
-          style={{
-            width: elementWidth + 26,
-            height: elementWidth + 26,
-            left: elementRect.left - elementWidth / 2,
-            top: elementRect.top - elementWidth / 2
-          }}
-          className={styles.IconWrapper}
-        >
-          <div
-            style={{
-              width: elementWidth + 14,
-              height: elementWidth + 14
-            }}
-            className={styles.IconInner}
-            dangerouslySetInnerHTML={{ __html: element.outerHTML }}
-          />
+        <div style={iconStyle} className={styles.IconWrapper}>
+          {element?.id === "guid-home-create-mobile" ? (
+            <div style={innerIconStyle} className={styles.IconInner}>
+              <div
+                style={{
+                  border: "3px solid #000",
+                  borderRadius: 50,
+                  backgroundColor: "#FF2681",
+                  width: 60,
+                  height: 60,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}
+                dangerouslySetInnerHTML={{ __html: element.outerHTML }}
+              />
+            </div>
+          ) : (
+            <div
+              style={innerIconStyle}
+              className={styles.IconInner}
+              dangerouslySetInnerHTML={{ __html: element.outerHTML }}
+            />
+          )}
         </div>
       )}
       {top !== undefined && left !== undefined && (
@@ -189,7 +227,8 @@ export const Mask: React.FC<MaskProps> = (props) => {
               elementWidth,
               elementHeight,
               contentWidth,
-              contentHeight
+              contentHeight,
+              isMobile
             )
           }}
         >
@@ -206,7 +245,7 @@ export const Mask: React.FC<MaskProps> = (props) => {
           >
             <path d="M0 0H15L7.5 9L0 0Z" fill="#FFF3F8" />
           </svg>
-          {getContent()}
+          <div className="w-full h-full relative">{renderMaskContent?.()}</div>
         </motion.div>
       )}
     </div>
--- app/components/layout/laptop/user/refer/modal/index.tsx
+++ app/components/layout/laptop/user/refer/modal/index.tsx
@@ -1,13 +1,15 @@
-import styles from './index.module.css';
-import { useReferStore } from '@/app/store/useRefer';
-import Modal from '@/app/components/modal';
-import { fail, success } from '@/app/utils/toast';
-import { useAccount } from '@/app/hooks/useAccount';
-import { Popup } from 'antd-mobile';
+import styles from "./index.module.css";
+import { useReferStore } from "@/app/store/useRefer";
+import Modal from "@/app/components/modal";
+import { fail, success } from "@/app/utils/toast";
+import { useAccount } from "@/app/hooks/useAccount";
+import { useGuidingTour } from "@/app/store/use-guiding-tour";
+import { Popup } from "antd-mobile";
 
 const ReferModal = (props: any) => {
   const { isMobile } = props;
   const store = useReferStore();
+  const { hasShownTour } = useGuidingTour();
 
   const handleClose = () => {
     store.setVisible(false);
@@ -20,19 +22,18 @@ const ReferModal = (props: any) => {
       onClose={handleClose}
       bodyStyle={{
         borderTopLeftRadius: "8px",
-        borderTopRightRadius: "8px",
+        borderTopRightRadius: "8px"
       }}
     >
       <ReferModalContent {...props} />
     </Popup>
-    ) : (
+  ) : (
     <Modal
-      open={store.visible}
+      open={store.visible && hasShownTour}
       onClose={handleClose}
-      style={{
-      }}
+      style={{}}
       mainStyle={{
-        border: 0,
+        border: 0
       }}
     >
       <ReferModalContent {...props} />
@@ -48,13 +49,14 @@ const ReferModalContent = (props: any) => {
 
   const handleCopy = () => {
     const shareLink = new URL(window?.location?.origin);
-    shareLink.searchParams.set('code', address ?? '');
-    navigator.clipboard.writeText(shareLink.toString())
+    shareLink.searchParams.set("code", address ?? "");
+    navigator.clipboard
+      .writeText(shareLink.toString())
       .then(() => {
-        success('Copied share link!');
+        success("Copied share link!");
       })
-      .catch(err => {
-        fail('Copy failed!');
+      .catch((err) => {
+        fail("Copy failed!");
       });
   };
 
@@ -65,69 +67,56 @@ const ReferModalContent = (props: any) => {
       </div>
       <div className={isMobile ? styles.ContentMobile : styles.Content}>
         <div className={isMobile ? styles.EarnedMobile : styles.Earned}>
-          <div className={isMobile ? styles.EarnedTitleMobile : styles.EarnedTitle}>
-            <div className={styles.EarnedTitleText}>
-              EARNED
-            </div>
-            <div className={isMobile ? styles.EarnedTitleValueMobile : styles.EarnedTitleValue}>
+          <div
+            className={isMobile ? styles.EarnedTitleMobile : styles.EarnedTitle}
+          >
+            <div className={styles.EarnedTitleText}>EARNED</div>
+            <div
+              className={
+                isMobile
+                  ? styles.EarnedTitleValueMobile
+                  : styles.EarnedTitleValue
+              }
+            >
               {userInfo?.referralFee || 0} SOL
             </div>
           </div>
           <div className={isMobile ? styles.ProgressMobile : styles.Progress}>
-            <div className={styles.ProgressValue} style={{ width: '25%' }} />
+            <div className={styles.ProgressValue} style={{ width: "25%" }} />
             <div className={styles.NodeList}>
               <div className={styles.NodeActive}>
-                <div className={styles.NodeLabel}>
-                  Vol.50k
-                </div>
+                <div className={styles.NodeLabel}>Vol.50k</div>
                 <div className={styles.NodeReward}>
-                  <div className={styles.NodeRewardLabel}>
-                    Earn Rewards
-                  </div>
+                  <div className={styles.NodeRewardLabel}>Earn Rewards</div>
                   <div className={styles.NodeRewardContent}>
-                    <div className={styles.NodeRewardValue}>
-                      0.4 SOL
-                    </div>
-                    <div className={styles.NodeRewardUnit}>
-                      /month
-                    </div>
+                    <div className={styles.NodeRewardValue}>0.4 SOL</div>
+                    <div className={styles.NodeRewardUnit}>/month</div>
                   </div>
                 </div>
               </div>
               <div className={styles.Node}>
-                <div className={styles.NodeLabel}>
-                  Vol.250k
-                </div>
+                <div className={styles.NodeLabel}>Vol.250k</div>
               </div>
               <div className={styles.Node}>
-                <div className={styles.NodeLabel}>
-                  Vol.500k
-                </div>
+                <div className={styles.NodeLabel}>Vol.500k</div>
                 <div className={styles.NodeReward}>
-                  <div className={styles.NodeRewardLabel}>
-                    Earn Rewards
-                  </div>
+                  <div className={styles.NodeRewardLabel}>Earn Rewards</div>
                   <div className={styles.NodeRewardContent}>
-                    <div className={styles.NodeRewardValue}>
-                      0.8 SOL
-                    </div>
-                    <div className={styles.NodeRewardUnit}>
-                      /month
-                    </div>
+                    <div className={styles.NodeRewardValue}>0.8 SOL</div>
+                    <div className={styles.NodeRewardUnit}>/month</div>
                   </div>
                 </div>
               </div>
               <div className={styles.Node}>
-                <div className={styles.NodeLabel}>
-                  Vol.1000k
-                </div>
+                <div className={styles.NodeLabel}>Vol.1000k</div>
               </div>
             </div>
           </div>
         </div>
         <div className={styles.Invite}>
           <div className={styles.InviteText}>
-            Invite Friends and Earn Commissions Get up {!isMobile && <br />} to <strong className={styles.InviteTextPrimary}>$5000</strong> rebates
+            Invite Friends and Earn Commissions Get up {!isMobile && <br />} to{" "}
+            <strong className={styles.InviteTextPrimary}>$5000</strong> rebates
           </div>
           <button
             type="button"
