c5d039b727f558102e89b4e685b5b2e0d4c3ac37
emily <emily@ref.finance>
2024-12-26 16:06:10 +0800

    fix mining.

--- app/sections/mining/mobile.tsx
+++ app/sections/mining/mobile.tsx
@@ -1,6 +1,14 @@
 import Content from "./content";
+import Gitbook from "@/app/components/icons/gitbook";
 import styles from "./ming.module.css";
 
 export default function Mining(props: any) {
-  return <Content styles={styles} isMobile={true} {...props} />;
+  return (
+    <>
+      <Content styles={styles} isMobile={true} {...props} />
+      {/* <a className={`button ${styles.Gitbook}`}>
+        <Gitbook />
+      </a> */}
+    </>
+  );
 }
