cc8fc4105e92a4dc80e0e713566422289ff82dce
emily <emily@ref.finance>
2024-12-31 18:04:24 +0800

    fix trend banner buying.

--- app/sections/trends/components/banner/index.tsx
+++ app/sections/trends/components/banner/index.tsx
@@ -1,10 +1,11 @@
-import { motion } from 'framer-motion';
-import styles from './index.module.css';
-import { useEffect, useState } from 'react';
-import { useTrends } from '@/app/sections/trends/hooks';
-import { numberFormatter } from '@/app/utils/common';
-import BuyModal from '@/app/sections/trends/components/buy';
-import { useTrade } from '@/app/sections/trends/hooks/trade';
+import { motion } from "framer-motion";
+import styles from "./index.module.css";
+import { useEffect, useState } from "react";
+import { useTrends } from "@/app/sections/trends/hooks";
+import { numberFormatter } from "@/app/utils/common";
+// import BuyModal from "@/app/sections/trends/components/buy";
+import TradeModal from "@/app/components/trade-modal";
+import { useTrade } from "@/app/sections/trends/hooks/trade";
 
 const TrendBanner = (props: any) => {
   const { isMobile, onClose } = props;
@@ -12,7 +13,9 @@ const TrendBanner = (props: any) => {
   const { top1 } = useTrends({ isPolling: true });
   const { tradeToken, onTrade, setTradeToken } = useTrade();
 
-  const [currentBg, setCurrentBg] = useState(isMobile ? BG_LIST_MOBILE[1] : BG_LIST[1]);
+  const [currentBg, setCurrentBg] = useState(
+    isMobile ? BG_LIST_MOBILE[1] : BG_LIST[1]
+  );
   const [currentBgIdx, setCurrentBgIdx] = useState(1);
   const [visible, setVisible] = useState(false);
 
@@ -40,7 +43,7 @@ const TrendBanner = (props: any) => {
     const timer = setInterval(setBg, 3000);
     return () => {
       clearInterval(timer);
-    }
+    };
   }, [currentBgIdx]);
 
   return (
@@ -49,85 +52,76 @@ const TrendBanner = (props: any) => {
         className={isMobile ? styles.ContainerMobile : styles.Container}
         initial={{ backgroundImage: isMobile ? BG_LIST_MOBILE[0] : BG_LIST[0] }}
         animate={{
-          backgroundImage: currentBg,
+          backgroundImage: currentBg
         }}
         transition={{
-          duration: 3,
+          duration: 3
         }}
       >
         <div
           className={styles.Avatar}
           style={{ backgroundImage: `url("${top1?.Icon}")` }}
         >
-          <img src="/img/trends/crown-laptop.svg" alt="" className={styles.Crown} />
+          <img
+            src="/img/trends/crown-laptop.svg"
+            alt=""
+            className={styles.Crown}
+          />
         </div>
         <div className={styles.Content}>
           <div className={styles.Title}>
-            <div className={styles.Name}>
-              {top1?.token_symbol}
-            </div>
-            {
-              !isMobile && (
-                <div className={styles.Rank}>
-                  [KING OF THE HILL]
-                </div>
-              )
-            }
-            {
-              isMobile && (
-                <div className={styles.TitleRight}>
-                  <BuyButton onBuy={handleBuy} />
-                  <button
-                    type="button"
-                    className={styles.CloseBtnMobile}
-                    onClick={onClose}
-                  />
-                </div>
-              )
-            }
+            <div className={styles.Name}>{top1?.token_symbol}</div>
+            {!isMobile && <div className={styles.Rank}>[KING OF THE HILL]</div>}
+            {isMobile && (
+              <div className={styles.TitleRight}>
+                <BuyButton onBuy={handleBuy} />
+                <button
+                  type="button"
+                  className={styles.CloseBtnMobile}
+                  onClick={onClose}
+                />
+              </div>
+            )}
           </div>
-          {
-            isMobile && (
-              <CreateTime top1={top1} />
-            )
-          }
+          {isMobile && <CreateTime top1={top1} />}
           <div className={styles.Summaries}>
             <div className={styles.Summary}>
-              <div className={styles.SummaryLabel}>
-                [Market Cap]
-              </div>
+              <div className={styles.SummaryLabel}>[Market Cap]</div>
               <div className={styles.SummaryValue}>
-                {numberFormatter(top1?.market_value, 2, true, { prefix: '$', isShort: true })}
+                {numberFormatter(top1?.market_value, 2, true, {
+                  prefix: "$",
+                  isShort: true
+                })}
               </div>
             </div>
             <div className={styles.Summary}>
-              <div className={styles.SummaryLabel}>
-                [Progress]
-              </div>
-              <div className={styles.SummaryValue}>
-                0%
-              </div>
+              <div className={styles.SummaryLabel}>[Progress]</div>
+              <div className={styles.SummaryValue}>0%</div>
             </div>
-            {
-              !isMobile && (
-                <CreateTime top1={top1} />
-              )
-            }
-            {
-              !isMobile && (
-                <div className={styles.TradeContainer}>
-                  <BuyButton onBuy={handleBuy} />
-                </div>
-              )
-            }
+            {!isMobile && <CreateTime top1={top1} />}
+            {!isMobile && (
+              <div className={styles.TradeContainer}>
+                <BuyButton onBuy={handleBuy} />
+              </div>
+            )}
           </div>
         </div>
       </motion.div>
-      <BuyModal
+      {/* <BuyModal
         visible={visible}
         tradeToken={tradeToken}
         onClose={handleBuyClose}
-      />
+      /> */}
+      {tradeToken && (
+        <TradeModal
+          show={visible}
+          onClose={() => {
+            handleBuyClose();
+          }}
+          data={tradeToken}
+          initType={"buy"}
+        />
+      )}
     </div>
   );
 };
@@ -140,7 +134,7 @@ const CreateTime = (props: any) => {
   return (
     <div className={styles.Summary}>
       <div className={styles.SummaryValue}>
-        Created in {top1?.created2Now?.replace?.(/ago$/, '')}
+        Created in {top1?.created2Now?.replace?.(/ago$/, "")}
       </div>
     </div>
   );
@@ -150,11 +144,7 @@ const BuyButton = (props: any) => {
   const { onBuy } = props;
 
   return (
-    <button
-      type="button"
-      className={styles.Trade}
-      onClick={onBuy}
-    >
+    <button type="button" className={styles.Trade} onClick={onBuy}>
       <img src="/img/trends/buy.svg" alt="" className={styles.TradeIcon} />
       <div className="">BUY</div>
     </button>
@@ -162,12 +152,12 @@ const BuyButton = (props: any) => {
 };
 
 const BG_LIST = [
-  'linear-gradient(90deg, #5900FF 0%, rgba(255, 38, 129, 0.00) 100%)',
-  'linear-gradient(90deg, #00E4A8 0%, rgba(255, 38, 129, 0.00) 100%)',
-  'linear-gradient(90deg, #FB00FF 0%, rgba(255, 38, 129, 0.00) 100%)',
+  "linear-gradient(90deg, #5900FF 0%, rgba(255, 38, 129, 0.00) 100%)",
+  "linear-gradient(90deg, #00E4A8 0%, rgba(255, 38, 129, 0.00) 100%)",
+  "linear-gradient(90deg, #FB00FF 0%, rgba(255, 38, 129, 0.00) 100%)"
 ];
 const BG_LIST_MOBILE = [
-  'linear-gradient(359deg, #60F 0.75%, rgba(255, 38, 129, 0.70) 98.88%)',
-  'linear-gradient(359deg, #0058E4 0.75%, rgba(255, 38, 129, 0.70) 98.88%)',
-  'linear-gradient(359deg, #FB00FF 0.75%, rgba(255, 38, 129, 0.70) 98.88%)',
+  "linear-gradient(359deg, #60F 0.75%, rgba(255, 38, 129, 0.70) 98.88%)",
+  "linear-gradient(359deg, #0058E4 0.75%, rgba(255, 38, 129, 0.70) 98.88%)",
+  "linear-gradient(359deg, #FB00FF 0.75%, rgba(255, 38, 129, 0.70) 98.88%)"
 ];
--- app/sections/trends/hooks/trade.ts
+++ app/sections/trends/hooks/trade.ts
@@ -1,25 +1,27 @@
-import { useState } from 'react';
-import { Trend } from '@/app/sections/trends/hooks/index';
+import { useState } from "react";
+import { Trend } from "@/app/sections/trends/hooks/index";
 
 export function useTrade() {
   const [tradeToken, setTradeToken] = useState<any>({});
 
   const onTrade = (token: Trend) => {
     if (!token) return;
+
     setTradeToken({
-      DApp: 'pump',
+      DApp: "pump",
       tokenName: token?.token_name,
       tokenSymbol: token?.token_symbol,
       tokenIcon: token?.Icon,
       ticker: token?.ticker,
       tokenDecimals: 18,
-      tickerAvatar: '',
+      tickerAvatar: "",
+      status: 1
     });
   };
 
   return {
     tradeToken,
     onTrade,
-    setTradeToken,
+    setTradeToken
   };
 }
