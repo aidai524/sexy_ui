790fc30f76afd4e2b470972719dc20977f2e4319
ben <benjm240@gmail.com>
2025-01-01 14:11:18 +0800

    fix 8887

--- app/components/tags/index.tsx
+++ app/components/tags/index.tsx
@@ -6,82 +6,84 @@ import { useRouter } from "next/navigation";
 import { useTokenTrade } from "@/app/hooks/useTokenTrade";
 
 interface Props {
-  data: Project;
+    data: Project;
 }
 
 export default function Tags({ data }: Props) {
-  const router = useRouter();
-  const [mc, setMc] = useState(0)
-  const userName = useMemo(() => {
-    console.log('data:', data)
+    const router = useRouter();
+    const [mc, setMc] = useState(0)
+    const userName = useMemo(() => {
+        console.log('data:', data)
 
-    if (data.creater) {
-        if (data.creater.name) {
-            return data.creater.name
-        }
-
-        if (data.creater.address) {
-            return formatAddress(data.creater.address);
-        }
-    }
-
-    if (data.account) {
-      return formatAddress(data.account);
-    }
-    return "-";
-  }, [data]);
-
-  const { getMC } = useTokenTrade({
-    tokenName: data.tokenName,
-    tokenSymbol: data.tokenSymbol as string,
-    tokenDecimals: data.tokenDecimals as number,
-    loadData: false
-  });
-
-  useEffect(() => {
-    if (data && data.DApp === 'sexy' && data.status === 1) {
-        getMC().then((res) => {
-            if (!isNaN(Number(res))) {
-                setMc(Number(res))
+        if (data.creater) {
+            if (data.creater.name) {
+                return data.creater.name
             }
-        })
-    }
-  }, [data])
 
-  return (
-    <div className={styles.tags}>
-        {
-            mc > 0 && <Tag><span className={ styles.mc }>Market Cap: { simplifyNum(mc) }</span></Tag>
+            if (data.creater.address) {
+                return formatAddress(data.creater.address);
+            }
         }
-      <Tag>Created in {data.time ? timeAgo(data.time) : 0}</Tag>
-      <Tag
-        onClick={() => {
-          router.push("/profile/user?account=" + data.account);
-        }}
-      >
-        <span>Created by</span>{" "}
-        <span className={styles.userName}>{userName}</span>
-      </Tag>
-    </div>
-  );
+
+        if (data.account) {
+            return formatAddress(data.account);
+        }
+        return "-";
+    }, [data]);
+
+    const { getMC } = useTokenTrade({
+        tokenName: data.tokenName,
+        tokenSymbol: data.tokenSymbol as string,
+        tokenDecimals: data.tokenDecimals as number,
+        loadData: false
+    });
+
+    useEffect(() => {
+        if (data && data.DApp === 'sexy' && data.status === 1) {
+            getMC().then((res) => {
+                if (!isNaN(Number(res))) {
+                    setMc(Number(res))
+                }
+            })
+        }
+    }, [data])
+
+    return (
+        <div className={styles.tags}>
+            {
+                mc > 0 && <Tag><span className={styles.mc}>Market Cap: {simplifyNum(mc)}</span></Tag>
+            }
+            {
+                data.status === 0 && <Tag>Created in {data.time ? timeAgo(data.time) : 0}</Tag>
+            }
+            <Tag
+                onClick={() => {
+                    router.push("/profile/user?account=" + data.account);
+                }}
+            >
+                <span>Created by</span>{" "}
+                <span className={styles.userName}>{userName}</span>
+            </Tag>
+        </div>
+    );
 }
 
 function Tag({
-  children,
-  onClick
+    children,
+    onClick
 }: {
-  children: React.ReactNode;
-  onClick?: () => void;
+    children: React.ReactNode;
+    onClick?: () => void;
 }) {
-  return (
-    <div
-      onClick={() => {
-        onClick && onClick();
-      }}
-      className={`${styles.tag} button`}
-    >
-      {" "}
-      {children}
-    </div>
-  );
+    return (
+        <div
+            onClick={() => {
+                onClick && onClick();
+            }}
+            className={`${styles.tag} button`}
+        >
+            {" "}
+            {children}
+        </div>
+    );
 }
