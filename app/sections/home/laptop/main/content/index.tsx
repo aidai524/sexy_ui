afb8d0531f5770cbaf866d082b1577b69ccfbb09
emily <emily@ref.finance>
2025-01-03 14:42:37 +0800

    optimize for tab switching on pc.

--- app/sections/home/laptop/main/content/index.tsx
+++ app/sections/home/laptop/main/content/index.tsx
@@ -48,7 +48,7 @@ export default function Content() {
           onOpenFull() {
             fullScreenStore.set({ isFull: true });
           },
-          list
+          list: list.current
         }}
       />
       {fullScreenStore?.isFull && (
