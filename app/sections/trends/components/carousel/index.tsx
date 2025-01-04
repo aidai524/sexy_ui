801b32e3e7f80e959b05684678adf03796f0b63a
jimmygu <jimmyguu2024@gmail.com>
2025-01-04 12:47:30 +0800

    fix: REF-9018

--- app/sections/trends/components/carousel/index.tsx
+++ app/sections/trends/components/carousel/index.tsx
@@ -1,15 +1,26 @@
+import { motion } from "framer-motion";
 import styles from "./index.module.css";
 
 export default function Carousel() {
   return (
     <div className={styles.CarouselContainer}>
       <div className={styles.Carousel}>
-        <div className={styles.CarouselInner}>
+        <motion.div
+          className={styles.CarouselInner}
+          animate={{
+            x: ['25%', 0],
+          }}
+          transition={{
+            repeat: Infinity,
+            ease: 'linear',
+            duration: 5,
+          }}
+        >
           <div className={styles.CarouselItem}>KING OF THE HILL</div>
           <div className={styles.CarouselItem}>KING OF THE HILL</div>
           <div className={styles.CarouselItem}>KING OF THE HILL</div>
           <div className={styles.CarouselItem}>KING OF THE HILL</div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
