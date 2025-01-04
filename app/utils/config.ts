6d996a7677a210d0bf59a6c347919a9bda6e4c4c
ben <benjm240@gmail.com>
2025-01-04 23:45:43 +0800

    test build

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -2,7 +2,6 @@ import { PublicKey, SystemProgram } from '@solana/web3.js';
 
 export const total_supply = 1000000000
 export const referral_address = 'EEYm1sXVhH1EpsUan6Sj31zdydALoAVCEYdVncJQJ8s6'
-// AYdgC17ymp4CkbbZ7pED2oa5hdnsp4dZVKJtt9542kFh
 export const programId_address = process.env.NEXT_PUBLIC_PROGRAM_ID || '67g5ZLhs2Nhobhm69u5vdxJutF5VesaB21G23RdXWshx'
 
 export const defaultAvatar = "/img/avatar.png";
