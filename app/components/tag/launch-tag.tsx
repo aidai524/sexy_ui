a30b2d8d40044429d396be7a1281b4d785e5a992
emily <emily@ref.finance>
2025-01-10 11:24:04 +0800

    add launch tag.

---
+++ app/components/tag/launch-tag.tsx
@@ -0,0 +1,21 @@
+import styles from "./launch-tag.module.css";
+
+export default function LaunchTag({ type }: { type: number }) {
+  if (type === 0) {
+    return (
+      <div className={styles.launchTag + " " + styles.launch1}>Pre-Launch</div>
+    );
+  }
+
+  if (type === 1) {
+    return (
+      <div className={styles.launchTag + " " + styles.launch2}>Launching</div>
+    );
+  }
+
+  if (type === 3) {
+    return (
+      <div className={styles.launchTag + " " + styles.launch3}>Launched</div>
+    );
+  }
+}
