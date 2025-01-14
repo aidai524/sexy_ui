8602e718409c062f76217d496d639fff7a9a0faa
jimmygu <jimmyguu2024@gmail.com>
2025-01-14 18:55:09 +0800

    fix: REF-9305 & REF-9304 & REF-9292

--- app/context/auth.tsx
+++ app/context/auth.tsx
@@ -3,7 +3,7 @@ import { useDebounceFn } from "ahooks";
 import { useUser } from "@/app/store/useUser";
 import useUserInfo from "@/app/hooks/useUserInfo";
 import { useAccount } from "@/app/hooks/useAccount";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useCodeStore, CODE } from "@/app/store/use-code";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { redirect } from "next/navigation";
@@ -24,6 +24,7 @@ export const AuthProvider: React.FC<{
   const [showLoginModal, setShowLoginModal] = useState(false);
   const { address, walletProvider } = useAccount();
   const userStore: any = useUser();
+  const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const codeStore: any = useCodeStore();
@@ -62,13 +63,15 @@ export const AuthProvider: React.FC<{
     window.disconnect = disconnect;
   }, []);
 
-  const logout = useCallback(async () => {
+  const logout = useCallback(async (isRedirect?: boolean) => {
     await disconnect?.();
     setUserInfo(undefined);
     userStore.set({
       userInfo: null
     });
     logOut();
+    // fix#REF-9292
+    isRedirect && router.replace('/');
   }, [address]);
 
   const updateCurrentUserInfo = useCallback(async () => {
