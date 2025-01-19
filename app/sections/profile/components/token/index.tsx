f2d292f5903be80c587aa87a2541e92336222f79
jimmygu <jimmyguu2024@gmail.com>
2025-01-19 10:35:37 +0800

    fix: REF-9503

--- app/sections/profile/components/token/index.tsx
+++ app/sections/profile/components/token/index.tsx
@@ -14,6 +14,7 @@ import { Program } from "@coral-xyz/anchor";
 import idl from "@/app/hooks/meme_launchpad.json";
 import { useConnection } from "@solana/wallet-adapter-react";
 import dayjs from "dayjs";
+import Media from '@/app/components/thumbnail/media';
 
 interface Props {
   data: Project;
@@ -199,9 +200,20 @@ export default function Token({
             router.push("/detail?address=" + data.address + "&from=profile");
           }}
         >
-          <img
-            className={styles.tokenImg}
-            src={data.tokenImg || "/img/token-placeholder.png"}
+          <Media
+            data={data}
+            imgHeight={84}
+            imgStyle={{
+              width: 84,
+              height: 84,
+            }}
+            style={{
+              overflow: "hidden",
+            }}
+            videoStyle={{
+              height: "100%",
+              background: "#000",
+            }}
           />
           <LaunchTag type={data.status as number} />
         </div>
