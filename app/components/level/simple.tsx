cce2202ed83e464ef9c5831718635d02a9a1c975
emily <emily@ref.finance>
2025-01-20 14:46:26 +0800

    fix bugs.

--- app/components/level/simple.tsx
+++ app/components/level/simple.tsx
@@ -1,10 +1,15 @@
 import styles from "./simple.module.css";
+import { getCurrentLevel } from "@/app/config";
 
 export default function Simple({ level }: any) {
   const _level = level || 1;
+  const currentLevel = getCurrentLevel(_level);
   return (
     <div
       className={`${styles.Label} ${level > 3 ? styles.Upper : styles.Normal}`}
+      style={{
+        backgroundImage: currentLevel.theme
+      }}
     >
       Lv.{_level}
     </div>
