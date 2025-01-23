798c55de1546c7012dbdc80038e10f556f5cd593
jimmygu <jimmyguu2024@gmail.com>
2025-01-23 23:05:25 +0800

    fix: remove coppies to profile

--- app/sections/profile/components/follower-actions/index.tsx
+++ app/sections/profile/components/follower-actions/index.tsx
@@ -21,10 +21,10 @@ export default function FollowerActions({ userInfo, style, onItemClick }: any) {
         <span className={styles.follwerAmount}>{userInfo?.following || 0}</span>
         <span>Following</span>
       </div>
-      <div className={styles.follwerItem}>
+      {/*<div className={styles.follwerItem}>
         <span className={styles.follwerAmount}>{userInfo?.likeNum}</span>
         <span>Coppied</span>
-      </div>
+      </div>*/}
     </div>
   );
 }
