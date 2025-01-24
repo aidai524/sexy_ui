4768d5715e3ed700a818fe6f174da1b6e6dcdfb3
emily <emily@ref.finance>
2025-01-24 15:58:02 +0800

    add redirecting to page.

--- app/airdrop/page.tsx
+++ app/airdrop/page.tsx
@@ -1,10 +1,14 @@
-import AirdropView from '@/app/sections/airdrop';
+"use client";
+import AirdropView from "@/app/sections/airdrop";
+import { useConfig } from "../store/useConfig";
+import { redirect } from "next/navigation";
 
 const Airdrop = () => {
-
-  return (
-    <AirdropView />
-  );
+  const configStore: any = useConfig();
+  if (!configStore.config.showAirdropEntry || configStore.config.airdropReady) {
+    redirect("/");
+  }
+  return <AirdropView />;
 };
 
 export default Airdrop;
