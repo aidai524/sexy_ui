c2716b6a54b83ae7214ff05d3587b795859f0a7e
emily <emily@ref.finance>
2025-01-31 22:02:41 +0800

    disable whitelist.

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -19,7 +19,7 @@ export default function Layout(props: any) {
   const configStore: any = useConfig();
   const { prepaidDelayTime, setPrepaidDelayTime } = usePrepaidDelayTimeStore();
   const pathname = usePathname();
-  useWhitelist();
+  // useWhitelist();
 
   const { getConfig } = useTokenTrade({
     tokenName: "",
