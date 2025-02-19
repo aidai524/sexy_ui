45c737e5fe6c8520d2d4bfd6269fef25f329017d
deep-path <476044723@qq.com>
2025-02-19 11:44:12 +0800

    feat: add card

--- app/sections/smart/components/cardContainer/index.tsx
+++ app/sections/smart/components/cardContainer/index.tsx
@@ -2,19 +2,31 @@ import React from 'react'
 import styles from './index.module.css'
 import TopTraderCard from '@/app/sections/smart/components/mTraderCard'
 import CopyTradeCard from '@/app/sections/smart/components/mCopyCard'
+import CopyCardEmpty from '@/app/sections/smart/components/mCopyCardEmpty'
 
 export default function CardContainer() {
   const isTopTrader = true;
+  const isCopyier = true;
   return (
     <div className={styles.container + ' ' + (isTopTrader ? styles.topTraderContainer : styles.copyTradeContainer)}>
        {
         isTopTrader ? (
           <>
             <TopTraderCard />
-            <CopyTradeCard />
+            {
+              isCopyier ? (
+                <CopyTradeCard />
+              ) : (
+                <CopyCardEmpty />
+              )
+            }
           </>
         ) : (
+           isCopyier ? (
             <CopyTradeCard />
+           ) : (
+            <CopyCardEmpty />
+           )
         )
        }
     </div>
