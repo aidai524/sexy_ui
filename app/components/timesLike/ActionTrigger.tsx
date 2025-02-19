a8e5ef874a4b9b699f10e3e8f36ab2b66ff2cf04
emily <emily@ref.finance>
2025-02-19 17:21:35 +0800

    fix like to earn.

--- app/components/timesLike/ActionTrigger.tsx
+++ app/components/timesLike/ActionTrigger.tsx
@@ -39,6 +39,7 @@ const onLike = async (data: any) => {
 
   return {
     likeNum: 0,
+    likeNumToday: 0,
     projectLikeNum: 0
   };
 };
@@ -52,9 +53,9 @@ const onHate = async (data: Project) => {
 };
 
 export async function actionLikeTrigger({ data, onShare, onSuccess }: any) {
-  const { likeNum, projectLikeNum } = await onLike(data);
+  const { likeNum, likeNumToday, projectLikeNum } = await onLike(data);
 
-  onSuccess(likeNum);
+  onSuccess(likeNumToday);
 
   if (projectLikeNum === 100) {
     const timeLikeHandler = Modal.show({
