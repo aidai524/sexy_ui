14d5319adbaef8cd649b1d8f0e0cef083017ab10
emily <emily@ref.finance>
2025-02-24 15:48:17 +0800

    trade and flip button.

--- app/sections/home/mobile/flip/index.tsx
+++ app/sections/home/mobile/flip/index.tsx
@@ -1,15 +1,19 @@
 import styles from "./index.module.css";
 import HomeIcon from "@/app/components/icons/home";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { useUserAgent } from "@/app/context/user-agent";
 
 const COLORS = ["#FBCA04", "#04FB1D", "#FE05D9"];
 
 export default function Flip({ token, id, onClick }: any) {
+  const { isMobile } = useUserAgent();
+  const [isEnter, setIsEnter] = useState(isMobile);
   if (token.account === window.sexAddress) {
     return null;
   }
 
-  return (
+  return isEnter ? (
     <motion.div
       key={id}
       className={styles.Container}
@@ -27,9 +31,23 @@ export default function Flip({ token, id, onClick }: any) {
         repeat: 30
       }}
       onClick={onClick}
+      onMouseLeave={() => {
+        setIsEnter(false);
+      }}
     >
       <HomeIcon size={28} type="black" />
       <div>Flip it!</div>
     </motion.div>
+  ) : (
+    <button
+      className={`button ${styles.Container}`}
+      style={{ backgroundColor: COLORS[0] }}
+      onMouseEnter={() => {
+        setIsEnter(true);
+      }}
+    >
+      <HomeIcon size={28} type="black" />
+      <div>Flip it!</div>
+    </button>
   );
 }
