b7d0fbe12479fb9fef4efd28d70b2f221cb45638
deep-path <476044723@qq.com>
2025-02-25 17:35:31 +0800

    feat: ui change

--- app/sections/smart/components/coppied/coppiedList/copyItemPc.tsx
+++ app/sections/smart/components/coppied/coppiedList/copyItemPc.tsx
@@ -1,18 +1,22 @@
 import React, { useCallback, useState, useEffect } from 'react'
-import styles from './pc.module.css'
+import styles from './index.module.css'
 import { defaultAvatar } from "@/app/utils/config";
 import { RingChart } from '../copyAmountPie';
 import Popover, { PopoverPlacement, PopoverTrigger } from '@/app/components/popover';
 import useUserInfo from '@/app/hooks/useUserInfo';
 import Big from 'big.js';
 import { useCopyTokenInfos } from '@/app/sections/profile/hooks/useCopyTokenInfos';
+import { numberFormatter } from '@/app/utils/common';
+import { formatAddress } from "@/app/utils";
+import { formatLongText } from '@/app/utils/common';
+import { formatDateTime } from '@/app/utils/index';
+import TokenGroup from '../tokenGroup';
 import MainBtn from '@/app/components/mainBtn';
-import { numberFormatter} from '@/app/utils/common';
 
-
-export default function CopyItemPc({itemInfo, handleCloseCopyTrade, isCloseCopyTradeLoading, handleCloseAndSell, handleClose, urlAddress}: any) {
+export default function CopyItem({itemInfo, handleCloseCopyTrade, isCloseCopyTradeLoading, urlAddress, handleCloseAndSell, handleClose,}: any) {
     const { userInfo: copyUserInfo } = useUserInfo(itemInfo?.from);
-    const tokenInfos = useCopyTokenInfos(itemInfo?.tokens);
+    const tokensInfo = useCopyTokenInfos(itemInfo?.tokens);
+    const [showTokenGroup, setShowTokenGroup] = useState(false);
     const commonStyles = {
         borderRadius: '30px',
         border: 'none',
@@ -31,22 +35,17 @@ export default function CopyItemPc({itemInfo, handleCloseCopyTrade, isCloseCopyT
     const commonStyles3 = {
         color: '#000',
     }
-    
   return (
     <div className={styles.ItemBox}>
-      
+      <div className={styles.ItemBoxContent}>
       {/* personal trade info */}
+      <div className={styles.PersonalTradeInfoBox}>
+        {/* personal info */}
         <div className={styles.PersonalInfo}>
             <img className={styles.Avatar} src={copyUserInfo?.icon  || defaultAvatar} alt="avatar" />
-            <div className={styles.publicBox}>
-                <p>Trader</p>
-                <div className={styles.Name}>@{copyUserInfo?.name || itemInfo?.from  || 'Flip'}</div>
-            </div>
+            <div className={styles.Name}>{formatLongText(copyUserInfo?.name) || formatAddress(itemInfo?.from) || 'Flip'}</div>
         </div>
-       
-     {/* copy info */}
-     <div className={styles.publicBox + " " + styles.CopyInfoBox}>
-        <p>Investment</p>
+        {/* copy info */}
         <div className={styles.CopyInfo}>
         <Popover
               content={
@@ -61,7 +60,7 @@ export default function CopyItemPc({itemInfo, handleCloseCopyTrade, isCloseCopyT
                   <p className={styles.TooltipItem}>
                     <span>Copying</span> 
                     <span className={styles.TooltipItemValue}>
-                    {numberFormatter(new Big(itemInfo?.netWorth).minus(itemInfo?.balance).toNumber() || 0, 4, true)}
+                      {numberFormatter(new Big(itemInfo?.netWorth).minus(itemInfo?.balance).toNumber() || 0, 4, true)}
                       <SolIconWithoutBg />
                     </span>
                   </p>
@@ -75,74 +74,68 @@ export default function CopyItemPc({itemInfo, handleCloseCopyTrade, isCloseCopyT
                 </div>
               }
               placement={PopoverPlacement.TopLeft}
-              trigger={PopoverTrigger.Hover}
+              trigger={tokensInfo?.length > 0 ? PopoverTrigger.Hover : undefined}
             >
             <div className={styles.CopyAmount}>
                 <p className={styles.CopyAmountText}>
                     <span className={styles.CopyAmountTextUseAmount}
                         style={{textDecoration: new Big(itemInfo?.netWorth).minus(itemInfo?.balance).toNumber() > 0 ? 'line-through' : 'none'}}
-                    >
-                        {numberFormatter(new Big(itemInfo?.netWorth).minus(itemInfo?.balance).toNumber() || 0, 4, true)}
-                    </span>
-                    <span className={styles.CopyAmountTextTotal}>/ {numberFormatter(itemInfo?.investment || 0, 4, true)}</span>
+                    >{numberFormatter(new Big(itemInfo?.netWorth).minus(itemInfo?.balance).toNumber() || 0, 4, true)}</span>
+                    <span className={styles.CopyAmountTextTotal}>/{numberFormatter(itemInfo?.investment || 0, 4, true)}</span>
                 </p>
                 <SolIconWithoutBg />
             </div>
-        </Popover>
-        <RingChart 
-            data={[
-                    { value: 
+            </Popover>
+              <div>
+                <RingChart data={[
+                        { value: 
                             new Big(itemInfo?.netWorth).minus(itemInfo?.balance).gte(0) ? 
                             new Big(itemInfo?.netWorth).minus(itemInfo?.balance).toNumber() : 0, 
                             color: '#C9FF5D', name: 'USED' 
                         },
-                  { value: itemInfo?.balance || 0, color: '#515B63', name: 'BALANCE' },
-        ]} />      
-        </div>      
-     </div>
-
-       {/* trade earn */}
-       <div className={styles.publicBox}>
-            <p style={{paddingTop: '18px'}}>Copied ROI (PNL) </p>
-           <div>
-           <div className={styles.PNLValuePercent}>{Big(itemInfo?.roi).times(100).toString() || 0}%</div>
-           <div className={styles.PNLValueUSD}>
-            <span style={{color: !itemInfo?.pnl.startsWith('-') ? '#C9FF5D' : '#FF5D5D'}}>{(numberFormatter(Big(itemInfo?.pnl).toString() || 0, 4, true) || '0')} SOL</span>
-           </div>
-           </div>
-        </div>  
-      {/* coppied tokens */}
-      <div className={styles.publicBox}>
-            <p style={{paddingTop: '8px'}}>Copied Tokens</p>
+                        { value: itemInfo?.balance || 0, color: '#515B63', name: 'BALANCE' },
+                ]} />
+              </div>
+        </div>
+      </div>
+      
+      <div className={styles.TradeInfoBox}>
+        {/* trade earn */}
+        <div className={styles.TradeEarn}>
+            <div className={styles.TitlePubStyle}>Copied ROI (PNL) </div>
+            <div className={styles.PNLValuePercent}>{Big(itemInfo?.roi).times(100).toString() || 0}%</div>
+            <div className={styles.PNLValueUSD}>
+              <span style={{color: !itemInfo?.pnl.startsWith('-') ? '#C9FF5D' : '#FF5D5D'}}>{(numberFormatter(Big(itemInfo?.pnl).toString() || 0, 4, true) || '0')} SOL</span>
+            </div>
+        </div>
+        {/* coppied tokens */}
+        <div className={styles.CoppiedTokens}>
+            <div className={styles.TitlePubStyle}>{itemInfo?.tokens?.length || 0} Copied Tokens</div>
             <Popover
-              content={
-                <div className={styles.Tooltip}>
-                      {tokenInfos.map((tokenInfo:any, index:number) => {
-                  return  <p className={styles.TooltipItem} key={index}>
-                            <span className={styles.TooltipItemIcon}>
-                                <img 
-                                    key={index} 
-                                    src={tokenInfo.icon || defaultAvatar} 
-                                    alt={tokenInfo.symbol || 'token'} 
-                                    title={tokenInfo.symbol || 'token'}
-                                />
-                            <span style={{fontSize: '12px'}}>{tokenInfo.symbol || 'token'}</span>
-                            </span>
-                            <span style={{color: '#fff',marginLeft: '12px', fontSize: '12px'}}>{numberFormatter(tokenInfo.balance || 0, 4, true)}</span>
-                         </p>
-                })}
-                 
-                </div>
-              }
-              placement={PopoverPlacement.TopLeft}
-              trigger={tokenInfos?.length > 0 ? PopoverTrigger.Hover : undefined}
-            >
-            <div className={styles.TokenIconBoxWrapper}>
-                <div className={styles.CopyAmountLength}>
-                {itemInfo?.tokens?.length || 0}
-                </div>
-                <div className={styles.TokenIconBox}>
-                {tokenInfos.map((tokenInfo:any, index:number) => {
+   content={
+     <div className={styles.Tooltip}>
+           {tokensInfo.map((tokenInfo:any, index:number) => {
+       return  <p className={styles.TooltipItem} key={index}>
+                 <span className={styles.TooltipItemIcon}>
+                     <img 
+                         key={index} 
+                         src={tokenInfo.icon || defaultAvatar} 
+                         alt={tokenInfo.symbol || 'token'} 
+                         title={tokenInfo.symbol || 'token'}
+                     />
+                 <span style={{fontSize: '12px'}}>{tokenInfo.symbol || 'token'}</span>
+                 </span>
+                 <span style={{color: '#fff',marginLeft: '12px', fontSize: '12px'}}>{numberFormatter(tokenInfo.balance || 0, 4, true)}</span>
+              </p>
+     })}
+      
+     </div>
+   }
+   placement={PopoverPlacement.TopLeft}
+   trigger={tokensInfo?.length > 0 ? PopoverTrigger.Hover : undefined}
+ >
+            <div className={styles.TokenIconBox} onClick={() => setShowTokenGroup(true)}>
+               {tokensInfo.map((tokenInfo:any, index:number) => {
                     if (index === 4) {
                         return <div key={index} className={styles.MoreTokens}>...</div>
                     }
@@ -155,31 +148,17 @@ export default function CopyItemPc({itemInfo, handleCloseCopyTrade, isCloseCopyT
                         />
                     }
                 })}
-                </div>
             </div>
             </Popover>
         </div>
-        {/* actions */}
-        <div className={styles.publicBox}>
-            <p style={{paddingTop: '8px'}}>Action</p>
-           <div className={styles.ActionButtonWrapper}>
-           {
-            !itemInfo?.tokens || itemInfo?.tokens?.length === 0 ?
-                <button 
-                className={styles.ActionButton}
-                disabled={itemInfo?.state === 5 || (isCloseCopyTradeLoading && itemInfo?.isClosing)}
-                onClick={async () => {
-                    itemInfo.isClosing = true;
-                        try {
-                            await handleClose(itemInfo);
-                        } finally {
-                            itemInfo.isClosing = false;
-                    }
-                }}
-            >
-                    {itemInfo?.state === 5 ? "Closing" : "Close"}
-                </button>
-            : 
+      </div>
+      </div>
+
+      {/* actions */}
+      <div className={styles.ActionButtonBox}>
+        <span className={styles.lastTradeAt}>{formatDateTime(itemInfo?.lastTradeAt)}</span>
+       {
+        !urlAddress && (
             <Popover
             content={
                 <div className={styles.ClosePopoverContent}>
@@ -198,21 +177,30 @@ export default function CopyItemPc({itemInfo, handleCloseCopyTrade, isCloseCopyT
             }
             placement={PopoverPlacement.BottomRight}
             trigger={PopoverTrigger.Click}
-        >
-            <button 
-                className={styles.ActionButton}
-                disabled={itemInfo?.state === 5 || (isCloseCopyTradeLoading && itemInfo?.isClosing)}
-            >
-                {itemInfo?.state === 5 ? "Closing" : "Close"}
-            </button>
-            </Popover>
-           
-            }   
-           </div>
-        </div>
+           >
+          <button 
+          className={styles.ActionButton}
+          disabled={itemInfo?.state === 5 || (isCloseCopyTradeLoading && itemInfo?.isClosing)}
+          onClick={async () => {
+              itemInfo.isClosing = true;
+              try {
+                  await handleCloseCopyTrade(itemInfo);
+              } finally {
+                  itemInfo.isClosing = false;
+              }
+          }}
+         >
+              {itemInfo?.state === 5 ? "Closing" : "Close"}
+          </button>
+          </Popover>
+        )
+       }
+      </div>
+      <TokenGroup show={showTokenGroup} onClose={() => setShowTokenGroup(false)} copiedInfo={itemInfo} />
     </div>
   )
 }
+
 
 
 const SolIconWithoutBg = () => {
