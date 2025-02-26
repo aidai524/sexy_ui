9bf3e5c09501e55b1373e386602e1df16721cf17
deep-path <476044723@qq.com>
2025-02-26 16:09:55 +0800

    fix: add walletaddress

--- app/sections/trends/hooks/useGetSmartMonies.ts
+++ app/sections/trends/hooks/useGetSmartMonies.ts
@@ -20,7 +20,7 @@ export const useGetSmartMonies = ({chain, page, pageSize, orderBy, walletAddress
 
   useEffect(() => {
     fetchSmartMonies();
-  }, [chain, page, pageSize, orderBy]);
+  }, [chain, page, pageSize, orderBy, walletAddress]);
 
   return { smartMonies, smartMoniesLoading };
 };
