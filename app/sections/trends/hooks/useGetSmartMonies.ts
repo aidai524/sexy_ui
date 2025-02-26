451e452f8a7b93e15104ee9d431789226f0b5c35
deep-path <476044723@qq.com>
2025-02-26 17:15:40 +0800

    fix: copying

--- app/sections/trends/hooks/useGetSmartMonies.ts
+++ app/sections/trends/hooks/useGetSmartMonies.ts
@@ -1,25 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import CopyTrade from '@/app/services/copyTrade';
+import { debounce } from 'lodash-es';
 
 export const useGetSmartMonies = ({chain, page, pageSize, orderBy, walletAddress}: {chain: string, page: number, pageSize: number, orderBy: string, walletAddress: string}) => {
   const copyTradeService = new CopyTrade();
   const [smartMonies, setSmartMonies] = useState<any>({items: [], total: 0});
   const [smartMoniesLoading, setSmartMoniesLoading] = useState(false);
 
-  const fetchSmartMonies = async () => {
-    setSmartMoniesLoading(true);
-    try {
-      const res = await copyTradeService.getSmartMonies({ chain, page, pageSize, orderBy, walletAddress });
-      setSmartMonies(res?.data || {items: [], total: 0});
-    } catch (error) {
-      setSmartMonies({items: [], total: 0});
-    } finally {
-      setSmartMoniesLoading(false);
-    }
-  };
+  //
+  const debouncedFetchSmartMonies = useCallback(
+    debounce(async () => {
+      try {
+        const res = await copyTradeService.getSmartMonies({ chain, page, pageSize, orderBy, walletAddress });
+        setSmartMonies(res?.data || {items: [], total: 0});
+      } catch (error) {
+        setSmartMonies({items: [], total: 0});
+      } finally {
+        setSmartMoniesLoading(false);
+      }
+    }, 350), //
+    [chain, page, pageSize, orderBy, walletAddress]
+  );
 
   useEffect(() => {
-    fetchSmartMonies();
+    setSmartMoniesLoading(true);
+    debouncedFetchSmartMonies();
+    //
+    return () => {
+      debouncedFetchSmartMonies.cancel();
+    };
   }, [chain, page, pageSize, orderBy, walletAddress]);
 
   return { smartMonies, smartMoniesLoading };
