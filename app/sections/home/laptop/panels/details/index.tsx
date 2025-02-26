144989d25c16eaac04f74b3dded343cf0b314b5d
emily <emily@ref.finance>
2025-02-26 11:35:56 +0800

    add details panel on detail.

--- app/sections/home/laptop/panels/details/index.tsx
+++ app/sections/home/laptop/panels/details/index.tsx
@@ -1,4 +1,6 @@
 import Info from "@/app/sections/detail/components/desc";
+import Summary from "@/app/sections/detail/components/info/summary";
+import PrelaunchStatus from "@/app/sections/detail/components/info/status/prelaunch";
 import useMcWithPump from "@/app/hooks/use-mc-with-pump";
 import styles from "./index.module.css";
 
@@ -6,6 +8,8 @@ export default function Details({ token }: any) {
   const mc = useMcWithPump(token);
   return (
     <div className={styles.Container}>
+      <Summary data={token} showAddress={true} from="panel" />
+      <PrelaunchStatus data={token} showAddress={true} />
       <Info mc={mc} data={token} from="panel" />
     </div>
   );
