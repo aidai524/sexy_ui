8a7070e4252e37737af1aaa636699208bc882211
deep-path <476044723@qq.com>
2025-03-03 15:14:17 +0800

    feat: add switch

--- app/smartDetail/page.tsx
+++ app/smartDetail/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import React from 'react'
 import SmartDetailContainer from '@/app/sections/smart/components/smartDetailContainer'
+import { SHOW_COPY_TRADE } from '@/app/utils/config';
 
 export default function SmartDetail() {
+  if (!SHOW_COPY_TRADE) {
+    return null
+  }
   return (
     <SmartDetailContainer />
   )
