b909b053a21a826156a88b25c55cde51f3817057
emily <emily@ref.finance>
2025-03-04 10:49:15 +0800

    remove https-proxy-agent for cloudflare

--- app/api/pump/route.ts
+++ app/api/pump/route.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import fetch from "node-fetch";
 import { URL } from "url";
-import { HttpsProxyAgent } from "https-proxy-agent";
 import { NextRequest } from "next/server";
 // const { spawn } = require('child_process');
 
@@ -11,10 +10,6 @@ export const runtime = "edge";
 
 export async function GET(request: Request | NextRequest) {
   const params: any = {};
-  if (process.env.NODE_ENV === "development") {
-    params.agent = new HttpsProxyAgent("http://127.0.0.1:4780");
-  }
-
   const parsedUrl = new URL(request.url as string);
 
   const token = parsedUrl.searchParams.get("token");
